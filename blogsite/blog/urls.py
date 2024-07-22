4a1355b46ba5e38d2a0803174da93a98f90d6ae4
Vatsal Changani <vatsalchangani@Vatsals-MacBook-Air.local>
2024-07-23 01:16:34 +0530

    path changes

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('entry/', views.entry, name='entry'),
     path('b_entry/', views.b_entry, name='b_entry'),
     path('blog/', views.blog_list.as_view()),
-    path('blog/<int:pk>/', views.blog_detail.as_view),
+    path('blog/<int:pk>/', views.blog_detail.as_view()),
 
 ]
